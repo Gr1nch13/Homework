ca0f4fcf3a0765ad7f67a2916fd9e7d4104decc8
Леонтьев Артем Анатольевич <aaleontiev@cbgr.ru>
2025-03-26 15:14:14 +0300

    ref Homework4

--- src/main/java/org/example/Lesson5/Homework4/MatrixProcessor.java
+++ src/main/java/org/example/Lesson5/Homework4/MatrixProcessor.java
@@ -5,14 +5,19 @@ import java.util.HashSet;
 public class MatrixProcessor {
     public static HashSet<Integer> findUniqueInMatrix(int[][] matrix){
         HashSet<Integer> uniqueElements = new HashSet<>();
+        HashSet<Integer> duplicateElements = new HashSet<>();
         if(matrix.length == 0){
             return uniqueElements;
         }
         for (int[] row : matrix){
             for (int element : row) {
-                uniqueElements.add(element);
+                if (!uniqueElements.add(element)) {
+                    uniqueElements.remove(element);
+                    duplicateElements.add(element);
+                }
             }
         }
+        uniqueElements.removeAll(duplicateElements);
         return uniqueElements;
     }
 }
